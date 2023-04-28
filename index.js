2084f626f0d1c18a4c11cea18214edfd32cb29a0
Sai kumar anapuram <55331069+saikumar0202@users.noreply.github.com>
2023-04-28 16:57:35 +0000

    text

--- index.js
+++ index.js
@@ -4,18 +4,17 @@ var canvas = new fabric.Canvas("canvas", {
 });
 
 // Add event listener for double-click on canvas
-canvas.on("touch:tap", function (options) {
-  addText(options);
-});
-canvas.on("mouse:dblclick", function (options) {
-  // Create a new text box at the double-clicked position
-  addText(options);
-});
-function addText(options) {
-  console.log(options);
+// canvas.on("touch:tap", function (options) {
+//   addText(options);
+// });
+// canvas.on("mouse:dblclick", function (options) {
+//   // Create a new text box at the double-clicked position
+//   addText(options);
+// });
+function addText() {
   var text = new fabric.IText("Enter text here", {
-    left: options.pointer.x,
-    top: options.pointer.y,
+    left: 200,
+    top: 200,
     fill: "black",
     // left: 100,
     // top: 100,
