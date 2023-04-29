e638c89da9bd2985ea24eca026482d282051c412
Sai kumar anapuram <55331069+saikumar0202@users.noreply.github.com>
2023-04-29 10:42:37 +0000

    .

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var isMobile = window.innerWidth < 500;
 var isTablet = window.innerWidth > 500 && window.innerWidth < 1000;
+
 var canvas = new fabric.Canvas("canvas", {
   width: isMobile ? 300 : isTablet ? 500 : 700,
   height: 300,
@@ -7,8 +8,8 @@ var canvas = new fabric.Canvas("canvas", {
   selection: !isMobile,
   stopContextMenu: true,
 });
+
 function addText() {
-  console.log(canvas);
   var text = new fabric.IText("Enter text here", {
     left: canvas.width / 2,
     top: canvas.height / 2 - 20,
@@ -35,27 +36,5 @@ function addText() {
   text.selectAll();
   text.enterEditing();
 }
-// canvas.on("object:scaling", function (obj) {
-//   console.log(obj);
-//   if (obj.target && obj.target.height && obj.target.scaleY) {
-//     let lastHeight;
 
-//     const updateTextSize = () => {
-//       if (obj.target) {
-//         if (obj.target.height && obj.target.scaleY) {
-//           lastHeight = obj.target.height * obj.target.scaleY;
-//         }
-
-//         obj.target.set({
-//           height: lastHeight || obj.target.height,
-//           scaleY: 1,
-//           scaleX: 1,
-//         });
-
-//         canvas.renderAll();
-//       }
-//     };
-
-//     updateTextSize();
-//   }
-// });
+addText();
